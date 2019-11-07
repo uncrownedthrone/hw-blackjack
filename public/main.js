d3dc0f7216a79e83b87158d986f2e380d298e0b0
Justin Swiger <justin.swiger@me.com>
2019-11-07 13:33:27 -0500

    DONE added deck to js TODO cards and buttons

--- public/main.js
+++ public/main.js
@@ -1,6 +1,56 @@
-const main = () => {
+const qs = (e) => document.querySelector(e)
 
+const ranks = [
+  'Ace',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  'Jack',
+  'Queen',
+  'King'
+]
+const suits = ['Hearts', 'Clubs', 'Spades', 'Diamonds']
+
+const deck = []
+const dealer = []
+const player = []
+let playerOneTotal = 0
+let playerTwoTotal = 0
+
+const getCardValue = (rank) => {
+  if (rank === 'Ace') {
+    return 11
+  } else if (rank === 'King' || rank === 'Queen' || rank === 'Jack') {
+    return 10
+  } else {
+    return parseInt(rank)
   }
+}
+
+const main = () => {
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < ranks.length; j++) {
+      const card = {
+        rank: ranks[j],
+        suit: suits[i],
+        value: getCardValue(ranks[j])
+      }
+      deck.push(card)
+    }
+  }
+  for (let i = 0; i < deck.length; i++) {
+    const j = Math.floor(Math.random() * 52)
+    const temp = deck[i]
+    deck[i] = deck[j]
+    deck[j] = temp
+  }
+  console.log(deck)
 }
 
 document.addEventListener('DOMContentLoaded', main)
