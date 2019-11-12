874245095b12797bdc35a4c5048baf3378c6115e
Justin Swiger <justin.swiger@me.com>
2019-11-11 22:29:26 -0500

    finished explorer mode

--- public/main.js
+++ public/main.js
@@ -1,40 +1,39 @@
-const qs = (e) => document.querySelector(e)
+const qs = e => document.querySelector(e);
 
 const ranks = [
-  'Ace',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '10',
-  'Jack',
-  'Queen',
-  'King'
-]
-const suits = ['Spades', 'Diamonds', 'Clubs', 'Hearts']
-const deck = []
+  "Ace",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "Jack",
+  "Queen",
+  "King"
+];
+const suits = ["Spades", "Diamonds", "Clubs", "Hearts"];
 
-const playerHand = []
-const dealerHand = []
-const show = true
-const hide = false
+const deck = [];
+const playerHand = [];
+const dealerHand = [];
 
-// getting the value of the cards
-const getCardValue = (rank) => {
-  if (rank === 'Ace') {
-    return 11
-  } else if (rank === 'King' || rank === 'Queen' || rank === 'Jack') {
-    return 10
+const show = true;
+const hide = false;
+
+const getCardValue = rank => {
+  if (rank === "Ace") {
+    return 11;
+  } else if (rank === "King" || rank === "Queen" || rank === "Jack") {
+    return 10;
   } else {
-    return parseInt(rank)
+    return parseInt(rank);
   }
-}
+};
 
-// shuffling deck and getting card images for each
 const shuffleDeck = () => {
   for (let i = 0; i < suits.length; i++) {
     for (let j = 0; j < ranks.length; j++) {
@@ -42,150 +41,179 @@ const shuffleDeck = () => {
         rank: ranks[j],
         suit: suits[i],
         value: getCardValue(ranks[j]),
-        imageUrl: ranks[j] + '_of_' + suits[i] + '.svg'
-      }
-      deck.push(card)
+        imageUrl: ranks[j] + "_of_" + suits[i] + ".svg"
+      };
+      deck.push(card);
     }
   }
   for (let i = 0; i < deck.length; i++) {
-    const j = Math.floor(Math.random() * 52)
-    const temp = deck[i]
-    deck[i] = deck[j]
-    deck[j] = temp
+    const j = Math.floor(Math.random() * 52);
+    const temp = deck[i];
+    deck[i] = deck[j];
+    deck[j] = temp;
   }
-  console.log(deck)
-  beginGame()
-}
+  console.log(deck);
+  beginGame();
+};
 
-// starts game and deals cards
-const beginGame = () => {
-  dealCard(deck, playerHand, '.playerHand', show)
-  dealCard(deck, playerHand, '.playerHand', show)
-  dealCard(deck, dealerHand, '.dealerHand', hide)
-  dealCard(deck, dealerHand, '.dealerHand', hide)
-
-  showSum(playerHand, '.playerSum')
-}
-
-// show the value of the players cards
-const showSum = (hand, sumContainer) => {
-  let playerSum = 0
-  for (let i = 0; i < hand.length; i++) {
-    playerSum += hand[i].value
-    console.log(playerSum)
-  }
-  qs(sumContainer).textContent = playerSum
-}
-
-// deals 2 cards face up for player and 2 face down for dealer
 const dealCard = (deckFrom, handTo, imageContainer, showHide) => {
-  handTo.push(deckFrom.pop())
+  handTo.push(deckFrom.pop());
 
-  const cardLi = document.createElement('li')
-  const img = document.createElement('img')
+  const cardLi = document.createElement("li");
+  const img = document.createElement("img");
   if (showHide) {
-    img.src = './images/cards/' + handTo[handTo.length - 1].imageUrl
-    img.alt = './images/cards/card_back.svg'
+    img.src = "./images/cards/" + handTo[handTo.length - 1].imageUrl;
+    img.alt = "./images/cards/card_back.svg";
   } else {
-    img.alt = './images/cards/' + handTo[handTo.length - 1].imageUrl
-    img.src = './images/cards/card_back.svg'
+    img.alt = "./images/cards/" + handTo[handTo.length - 1].imageUrl;
+    img.src = "./images/cards/card_back.svg";
   }
-  cardLi.appendChild(img)
-  qs(imageContainer).appendChild(cardLi)
-}
+  cardLi.appendChild(img);
 
-// dealer shows cards when player hits stand
-const flipCard = (imageContainer) => {
-  for (let i = 0; i < qs(imageContainer).children.length; i++) {
-    const img = qs(imageContainer).children[i].children[0]
-    const temp = img.src
-    img.src = img.alt
-    img.alt = temp
+  qs(imageContainer).appendChild(cardLi);
+};
+
+const showSum = (hand, sumContainer) => {
+  let playerSum = 0;
+  for (let i = 0; i < hand.length; i++) {
+    playerSum += hand[i].value;
   }
-}
+  qs(sumContainer).textContent = playerSum;
+  return playerSum;
+};
 
-// dealer plays his hand when player stands
 const dealerPlays = () => {
-  flipCard('.dealerHand')
-  showSum(dealerHand, '.dealerSum')
-  qs('.standButton').disabled = true
-  qs('.hitButton').disabled = true
-  console.log(dealerHand)
-  gameEnd()
-}
+  flipCard(".dealerHand");
+  while (showSum(dealerHand, ".dealerSum") < 17) {
+    dealCard(deck, dealerHand, ".dealerHand", show);
+  }
+  checkScore();
+  qs(".hitButton").disabled = true;
+  qs(".standButton").disabled = true;
+};
+
+const flipCard = imageContainer => {
+  for (let i = 0; i < qs(imageContainer).children.length; i++) {
+    const img = qs(imageContainer).children[i].children[0];
+    const temp = img.src;
+    img.src = img.alt;
+    img.alt = temp;
+  }
+};
+
+const beginGame = () => {
+  dealCard(deck, playerHand, ".playerHand", show);
+  dealCard(deck, dealerHand, ".dealerHand", hide);
+  dealCard(deck, playerHand, ".playerHand", show);
+  dealCard(deck, dealerHand, ".dealerHand", hide);
+
+  if (showSum(playerHand, ".playerSum") == 21) {
+    dealerPlays();
+  }
+};
+
+const checkScore = () => {
+  const playerSum = showSum(playerHand, ".playerSum");
+  const dealerSum = showSum(dealerHand, ".dealerSum");
+  if (dealerSum > 21) {
+    qs(".playerSum").textContent = playerSum + " - WINS";
+    qs(".dealerSum").textContent = dealerSum + " - BUSTS";
+    qs(".hitButton").disabled = true;
+    qs(".standButton").disabled = true;
+    qs(".dealerHitButton").disabled = true;
+  } else if (playerSum > 21) {
+    qs(".playerSum").textContent = playerSum + " - BUSTS";
+    qs(".dealerSum").textContent = dealerSum + " - WINS";
+    qs(".hitButton").disabled = true;
+    qs(".dealerHitButton").disabled = true;
+    qs(".standButton").disabled = true;
+  } else if (dealerSum > playerSum) {
+    qs(".playerSum").textContent = playerSum + " - LOSES";
+    qs(".dealerSum").textContent = dealerSum + " - WINS";
+    qs(".hitButton").disabled = true;
+    qs(".standButton").disabled = true;
+    qs(".dealerHitButton").disabled = true;
+  } else if (dealerSum < playerSum) {
+    qs(".playerSum").textContent = playerSum + " - WINS";
+    qs(".dealerSum").textContent = dealerSum + " - LOSES";
+    qs(".hitButton").disabled = true;
+    qs(".standButton").disabled = true;
+    qs(".dealerHitButton").disabled = true;
+  } else if (dealerHand.length != playerHand.length && dealerSum === 21) {
+    qs(".playerSum").textContent = playerSum + " - PUSH";
+    qs(".dealerSum").textContent = dealerSum + " - PUSH";
+    qs(".hitButton").disabled = true;
+    qs(".standButton").disabled = true;
+    qs(".dealerHitButton").disabled = true;
+    if (dealerHand.length == 2) {
+      qs(".playerSum").textContent = playerSum + " - LOSES";
+      qs(".dealerSum").textContent = dealerSum + " - WINS";
+      qs(".hitButton").disabled = true;
+      qs(".standButton").disabled = true;
+      qs(".dealerHitButton").disabled = true;
+    } else if (playerHand.length == 2) {
+      qs(".playerSum").textContent = playerSum + " - WINS";
+      qs(".dealerSum").textContent = dealerSum + " - LOSES";
+      qs(".hitButton").disabled = true;
+      qs(".standButton").disabled = true;
+      qs(".dealerHitButton").disabled = true;
+    } else {
+      qs(".playerSum").textContent = playerSum + " - PUSH";
+      qs(".dealerSum").textContent = dealerSum + " - PUSH";
+      qs(".hitButton").disabled = true;
+      qs(".standButton").disabled = true;
+      qs(".dealerHitButton").disabled = true;
+    }
+  } else {
+    qs(".playerSum").textContent = playerSum + " - PUSH";
+    qs(".dealerSum").textContent = dealerSum + " - PUSH";
+    qs(".hitButton").disabled = true;
+    qs(".standButton").disabled = true;
+    qs(".dealerHitButton").disabled = true;
+  }
+  qs(".hitButton").disabled = true;
+  qs(".standButton").disabled = true;
+};
 
 const hitPlayer = () => {
-  const hitPlayerHand = deck.pop()
-  playerHand.push(hitPlayerHand)
-  const playerHandLiTwo = document.createElement('li')
-  const imgTwo = document.createElement('img')
-  imgTwo.src = './images/cards/' + hitPlayerHand.imageUrl
-  playerHandLiTwo.appendChild(imgTwo)
-  qs('.playerHand').appendChild(playerHandLiTwo)
-  showSum(playerHand, '.playerSum')
-}
+  const hitPlayerHand = deck.pop();
+  playerHand.push(hitPlayerHand);
+  const playerHandLiTwo = document.createElement("li");
+  const imgTwo = document.createElement("img");
+  imgTwo.src = "./images/cards/" + hitPlayerHand.imageUrl;
+  playerHandLiTwo.appendChild(imgTwo);
+  qs(".playerHand").appendChild(playerHandLiTwo);
+
+  const playerSum = showSum(playerHand, ".playerSum");
+  if (playerSum == 21) {
+    dealerPlays();
+  } else if (playerSum > 21) {
+    flipCard(".dealerHand");
+    showSum(dealerHand, ".dealerSum");
+    qs(".winner").textContent = "Dealer Wins";
+    qs(".hitButton").disabled = true;
+    qs(".standButton").disabled = true;
+  }
+};
 
 const hitDealer = () => {
-  const hitDealerHand = deck.pop()
-  dealerHand.push(hitDealerHand)
-  const dealerHandLiTwo = document.createElement('li')
-  const imgTwo = document.createElement('img')
-  imgTwo.src = './images/cards/' + hitDealerHand.imageUrl
-  dealerHandLiTwo.appendChild(imgTwo)
-  qs('.dealerHand').appendChild(dealerHandLiTwo)
-  showSum(dealerHand, '.dealerSum')
-  gameEnd()
-}
-
-const gameEnd = () => {
-  // const playerHand = qs('.playerSum').value
-  // const dealerHand = qs('.dealerSum').value
-  if (playerHand > 21) {
-    qs('.playerSum').textContent = 'BUSTS - 1'
-    qs('.dealerSum').textContent = 'WIN'
-    qs('.hitButton').disabled = true
-    qs('.dealerHitButton').disabled = true
-    qs('.standButton').disabled = true
-  } else if (playerHand === 21) {
-    qs('.playerSum').textContent = 'WIN - 2'
-    qs('.dealerSum').textContent = 'LOSE'
-    qs('.hitButton').disabled = true
-    qs('.standButton').disabled = true
-  } else if (dealerHand >= 17) {
-    qs('.dealerHitButton').disabled = true
-  } else if (dealerHand > 21) {
-    qs('.playerSum').textContent = 'WIN - 3'
-    qs('.dealerSum').textContent = 'LOSE'
-    qs('.hitButton').disabled = true
-    qs('.standButton').disabled = true
-    qs('.dealerHitButton').disabled = true
-  } else if (dealerHand === 21) {
-    qs('.playerSum').textContent = 'LOSE - 4'
-    qs('.dealerSum').textContent = 'WIN'
-    qs('.hitButton').disabled = true
-    qs('.standButton').disabled = true
-    qs('.dealerHitButton').disabled = true
-  } else if (dealerHand > playerHand) {
-    qs('.playerSum').textContent = 'WINNER - 5'
-    qs('.dealerSum').textContent = 'LOSE'
-    qs('.hitButton').disabled = true
-    qs('.standButton').disabled = true
-    qs('.dealerHitButton').disabled = true
-  } else if (dealerHand === playerHand) {
-    qs('.playerSum').textContent = 'PUSH - 6'
-    qs('.dealerSum').textContent = 'PUSH'
-    qs('.hitButton').disabled = true
-    qs('.standButton').disabled = true
-    qs('.dealerHitButton').disabled = true
+  for (let n = 0; n < 1; n++) {
+    const hitDealerHand = deck.pop();
+    dealerHand.push(hitDealerHand);
+    const dealerHandLiTwo = document.createElement("li");
+    const imgTwo = document.createElement("img");
+    imgTwo.src = "./images/cards/" + hitDealerHand.imageUrl;
+    dealerHandLiTwo.appendChild(imgTwo);
+    qs(".dealerHand").appendChild(dealerHandLiTwo);
+    showSum(dealerHand, ".dealerSum");
   }
-}
+};
 
-const resetGame = () => {
-  location.reload()
-}
+const replay = () => {
+  location.reload();
+};
 
-document.addEventListener('DOMContentLoaded', shuffleDeck)
-qs('.standButton').addEventListener('click', dealerPlays)
-qs('.hitButton').addEventListener('click', hitPlayer)
-qs('.dealerHitButton').addEventListener('click', hitDealer)
-qs('.resetButton').addEventListener('click', resetGame)
+document.addEventListener("DOMContentLoaded", shuffleDeck);
+qs(".standButton").addEventListener("click", dealerPlays);
+qs(".hitButton").addEventListener("click", hitPlayer);
+qs(".resetButton").addEventListener("click", replay);
